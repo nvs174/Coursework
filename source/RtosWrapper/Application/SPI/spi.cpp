f3b47e1a48bc3a47444557956ede30123a36b08f
nvs174 <nejur02@mail.ru>
2024-04-20 18:58:09 +0300

    Update spi.cpp

--- source/RtosWrapper/Application/SPI/spi.cpp
+++ source/RtosWrapper/Application/SPI/spi.cpp
@@ -8,16 +8,15 @@ class Spi: public ISpi
 public:
   void ModeWrite(uint8_t reg, uint8_t value) override 
   {
-        
-  uint8_t tx[1]={0};
-  uint8_t rx[1] = {0};
-  tx[0] = reg | 0x7F;
-  rx[0] = value;
-  GPIOB::ODR::ODR12::Low::Set();
-  SPI2::DR::Write(tx[0]);
-  SPI2::DR::Write(rx[0]);
-  while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-  GPIOB::ODR::ODR12::High::Set();
+    tx[1]={0};
+    rx[1] = {0};
+    tx[0] = reg | 0x7F;
+    rx[0] = value;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    SPI2::DR::Write(rx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
   }
   
   
@@ -25,6 +24,7 @@ public:
   
   int8_t ModeRead(uint8_t reg) override 
   {
+    tx[1]={0};
     tx[0]= reg | 0x80;
     GPIOB::ODR::ODR12::Low::Set();
     SPI2::DR::Write(tx[0]);
@@ -34,8 +34,9 @@ public:
   }
  
 private:
-  uint8_t tx[28]={0,};
-  //uint8_t rxarray[28]={0,};
+  uint8_t tx[1];
+  uint8_t rx[1];
+ 
 };
 
 #endif
