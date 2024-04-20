82239a6da74ce8c52ca902f988dd1c7fd43700df
nvs174 <nejur02@mail.ru>
2024-04-20 18:31:04 +0300

    Create spi.cpp

---
+++ source/RtosWrapper/Application/SPI/spi.cpp
@@ -0,0 +1,56 @@
+#ifndef SPI
+#define SPI
+#include "ispi.h"
+#include "spi2registers.hpp" // for SPI2
+#include "gpiobregisters.hpp"  // for GPIOB
+class Spi: public ISpi 
+{
+public:
+  void ModeWrite(uint8_t reg, uint8_t value) override 
+  {
+        
+  uint8_t tx[1]={0};
+  uint8_t rx[1] = {0};
+  tx[0] = reg | 0x7F;
+  rx[0] = value;
+  GPIOB::ODR::ODR12::Low::Set();
+  SPI2::DR::Write(tx[0]);
+  SPI2::DR::Write(rx[0]);
+  while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+  GPIOB::ODR::ODR12::High::Set();
+    
+    
+    //tx[0]= reg | 0x7F;
+    //GPIOB::ODR::ODR12::Low::Set();
+    //SPI2::DR::Write(tx[0]);
+    //while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    //GPIOB::ODR::ODR12::High::Set();
+    //delay(5);
+  }
+  
+  
+  
+  
+  int8_t ModeRead(uint8_t reg) override 
+  {
+    tx[0]= reg | 0x80;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
+    delay(5);
+    return static_cast<std::uint8_t> (SPI2::DR::Get());
+  }
+  void delay(int cycles)
+{
+  for(int i = 0; i < cycles; ++i)
+  {
+    asm volatile("");
+  }
+}
+private:
+  uint8_t tx[28]={0,};
+  //uint8_t rxarray[28]={0,};
+};
+
+#endif
