6fdadd589a2a756d4b34d5dae165d0ab3440d892
nvs174 <nejur02@mail.ru>
2024-04-20 19:21:01 +0300

    Update ispi.h

--- source/RtosWrapper/Application/SPI/ispi.h
+++ source/RtosWrapper/Application/SPI/ispi.h
@@ -6,5 +6,10 @@ class ISpi
 public:
   virtual void ModeWrite(uint8_t reg, uint8_t value) = 0;
   virtual int8_t ModeRead(uint8_t reg) = 0;
+  virtual uint16_t ModeReadGidT1(uint8_t reg) = 0;
+  virtual int16_t ModeReadGidT3(uint8_t reg) = 0;
+  virtual int16_t ModeReadGidT3(uint8_t reg) = 0;
+  virtual int32_t ModeReadTempPress(uint8_t Reg) = 0;
+  virtual int16_t ModeReadHam(uint8_t Reg) = 0;
 };
 #endif
