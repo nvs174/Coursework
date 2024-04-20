2815719b8332811032dcfd064dcc21dd401c80e8
nvs174 <nejur02@mail.ru>
2024-04-20 19:21:03 +0300

    Update spi.cpp

--- source/RtosWrapper/Application/SPI/spi.cpp
+++ source/RtosWrapper/Application/SPI/spi.cpp
@@ -19,9 +19,7 @@ public:
     GPIOB::ODR::ODR12::High::Set();
   }
   
-  
-  
-  
+
   int8_t ModeRead(uint8_t reg) override 
   {
     tx[1]={0};
@@ -32,7 +30,55 @@ public:
     GPIOB::ODR::ODR12::High::Set();
     return static_cast<std::uint8_t> (SPI2::DR::Get());
   }
+  
+  
+  uint16_t ModeReadGidT1(uint8_t reg) override 
+  {
+    tx[1]={0};
+    tx[0]= reg | 0x80;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
+    return static_cast<std::uint16_t> (SPI2::DR::Get());
+  }
+  
+  int16_t ModeReadGidT3(uint8_t reg) override 
+  {
+    tx[1]={0};
+    tx[0]= reg | 0x80;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
+    return static_cast<std::int16_t> (SPI2::DR::Get());
+  }
  
+  int32_t ModeReadTempPress(uint8_t Reg) override 
+  {
+    tx[1]={0};
+    tx[0]= reg | 0x80;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
+    return static_cast<std::int32_t> (SPI2::DR::Get());
+  
+  }
+  
+  
+  int16_t ModeReadHam(uint8_t Reg) override 
+  {
+    tx[1]={0};
+    tx[0]= reg | 0x80;
+    GPIOB::ODR::ODR12::Low::Set();
+    SPI2::DR::Write(tx[0]);
+    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
+    GPIOB::ODR::ODR12::High::Set();
+    return static_cast<std::int16_t> (SPI2::DR::Get());
+  }
+  
+  
 private:
   uint8_t tx[1];
   uint8_t rx[1];
