f6db7d4874b766b7df7668862816a8a7fb91a0c8
nvs174 <nejur02@mail.ru>
2024-04-20 20:30:10 +0300

    Update main.cpp

--- source/RtosWrapper/main.cpp
+++ source/RtosWrapper/main.cpp
@@ -1,33 +1,19 @@
 #include "rtos.hpp"         // for Rtos
-#include "mailbox.hpp"      // for Mailbox
-#include "event.hpp"        // for Event
-
 #include "rccregisters.hpp" // for RCC
-
 #include "gpiobregisters.hpp"  // for GPIOB
-
-#include "spi2registers.hpp" // for SPI2
 #include "spi2fieldvalues.hpp" // for SPI2
-
-
 #include "registerbme280.h" // for register BME280
 #include "imeasurementscontroller.h" // for update register
 #include "ispi.h" // for ISPI
 
-
-
-
-
 std::uint32_t SystemCoreClock = 16'000'000U;
-
 
 extern "C" {
 int __low_level_init(void)
 {
   RCC::AHB1ENR::GPIOBEN::Enable::Set();
   RCC::APB1ENR::SPI2EN::Enable::Set();
-  
-  GPIOB::MODER::MODER12::Output::Set();
+  GPIOB::MODER::MODER12::Output::Set(); // TODO исправлю в конце 
   GPIOB::ODR::ODR12::High::Set();
   SPI2::CR1::MSTR::Master::Set();  
   SPI2::CR1::RXONLY::FullDuplex::Set(); 
@@ -40,24 +26,14 @@ int __low_level_init(void)
   SPI2::CR2::FRF::MotorolaMode::Set();
   SPI2::CR1::CRCEN::CrcCalcDisable::Set();
   SPI2::CR1::SPE::Enable::Set();
-  
-  
-
   return 1;
 }
 }
-
-
 Spi spi;
-
 Bme280 bme (spi);
 MeasureTask measureTask (bme);
-
-
 int main()
 {
-    
-  
   using namespace OsWrapper;
   Rtos::CreateThread(measureTask, "measureTask");
 
