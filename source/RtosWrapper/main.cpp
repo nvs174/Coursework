47a3c03ec5d167326037d42146e7fc182691fd65
nvs174 <nejur02@mail.ru>
2024-04-20 19:32:35 +0300

    Update main.cpp

--- source/RtosWrapper/main.cpp
+++ source/RtosWrapper/main.cpp
@@ -10,19 +10,11 @@
 #include "spi2fieldvalues.hpp" // for SPI2
 
 
-#include "ifloatdataprovider.h" // for return of measured parameters.
-#include "imeasurementsupdate.h" // for updating of measured parameters.
-#include "temperature.h" // for temperature BME280
 #include "registerbme280.h" // for register BME280
-#include "bme280.h"         // for BME280
 #include "imeasurementscontroller.h" // for update register
 #include "ispi.h" // for ISPI
-#include "spi.h" // for SPIMode
-#include "measuretask.h"
 
-#include "idatanotifyt.h"
-#include "idatanotifyp.h"
-#include "idatanotifyh.h"
+
 
 
 
@@ -33,12 +25,10 @@ extern "C" {
 int __low_level_init(void)
 {
   RCC::AHB1ENR::GPIOBEN::Enable::Set();
+  RCC::APB1ENR::SPI2EN::Enable::Set();
+  
   GPIOB::MODER::MODER12::Output::Set();
   GPIOB::ODR::ODR12::High::Set();
-
-  
-  
-  
   SPI2::CR1::MSTR::Master::Set();  
   SPI2::CR1::RXONLY::FullDuplex::Set(); 
   SPI2::CR1::DFF::Data8bit::Set();
