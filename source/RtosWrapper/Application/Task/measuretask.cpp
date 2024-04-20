14f37d56a86f81968f2bd39896723bcf477d6296
nvs174 <nejur02@mail.ru>
2024-04-20 20:30:07 +0300

    Update measuretask.cpp

--- source/RtosWrapper/Application/Task/measuretask.cpp
+++ source/RtosWrapper/Application/Task/measuretask.cpp
@@ -4,22 +4,18 @@
 #include "thread.hpp" // for OsWrapper::Thread<>
 #include "imeasurementscontroller.h" // for update register
 
-
 class MeasureTask: public OsWrapper::Thread<128> 
 {
 public:
-    MeasureTask(IMeasurementsController& controller) : mController(controller){}
-  virtual void Execute() override 
+  MeasureTask(IMeasurementsController& controller) : mController(controller){}
+  void Execute() override 
   {
     for (;;) 
     {
       mController.Update();
       Sleep(100ms);
     }
-  
   }
-  
-  
 private:
   IMeasurementsController& mController;
 };
