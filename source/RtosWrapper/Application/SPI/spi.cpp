c67feb349ebcba99c9deb7ac48a15536996eb3f9
nvs174 <nejur02@mail.ru>
2024-04-23 13:17:33 +0300

    Update spi.cpp

--- source/RtosWrapper/Application/SPI/spi.cpp
+++ source/RtosWrapper/Application/SPI/spi.cpp
@@ -8,28 +8,28 @@ public:
   
   void WriteByte(uint8_t reg,uint8_t valu) override 
   {
-    std::cout << "записали бит в указанный регистр" << std::endl; // TODO убрать
+    std::cout << "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << std::endl; // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
   }
   uint8_t ReadByte(uint8_t reg) override 
   {
-    std::cout << "считали байт указанного регистра" << std::endl; // TODO убрать
+    std::cout << "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ" << std::endl; // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
     return 1;
   }
   void ReadWord(uint8_t reg, int16_t *value) override 
   {
-    std::cout << "Dig_T3, влажность есть (2)" << std::endl; // TODO убрать
+    std::cout << "Dig_T3, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ (2)" << std::endl; // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
     *value = 1;
   }
   
   
   void ReadWord(uint8_t reg, uint16_t *value) override 
   {
-    std::cout << "Dig_T1, влажность есть (1)" << std::endl; // TODO убрать
+    std::cout << "Dig_T1, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ (1)" << std::endl; // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
     *value = 2;
   }
-  void ReadWord(uint8_t reg, int32_t *value) override // TODO убрать
+  void ReadWord(uint8_t reg, int32_t *value) override // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
   {
-    std::cout << "давление, темп есть (3)" << std::endl; // TODO убрать
+    std::cout << "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ (3)" << std::endl; // TODO пїЅпїЅпїЅпїЅпїЅпїЅ
     *value = 3;
   }
   
