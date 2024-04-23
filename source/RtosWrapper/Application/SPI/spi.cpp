b7e670968ad5b75b901eee3e903035adf4d6c730
nvs174 <nejur02@mail.ru>
2024-04-23 13:07:24 +0300

    Update spi.cpp

--- source/RtosWrapper/Application/SPI/spi.cpp
+++ source/RtosWrapper/Application/SPI/spi.cpp
@@ -1,88 +1,41 @@
 #ifndef SPI
 #define SPI
-#include "ispi.h" // for ModSPI
-#include "spi2registers.hpp" // for SPI2
-#include "gpiobregisters.hpp"  // for GPIOB
+#include "ispi.h" // for mode Spi
+
 class Spi: public ISpi 
 {
 public:
-  void ModeWrite(uint8_t reg, uint8_t value) override 
-  {
-    tx[1]={0};
-    rx[1] = {0};
-    tx[0] = reg | 0x7F;
-    rx[0] = value;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    SPI2::DR::Write(rx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-  }
   
-
-  int8_t ModeRead(uint8_t reg) override 
+  void WriteByte(uint8_t reg,uint8_t valu) override 
   {
-    tx[1]={0};
-    tx[0]= reg | 0x80;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-    return static_cast<std::uint8_t> (SPI2::DR::Get());
+    std::cout << "записали бит в указанный регистр" << std::endl; // TODO убрать
+  }
+  uint8_t ReadByte(uint8_t reg) override 
+  {
+    std::cout << "считали байт указанного регистра" << std::endl; // TODO убрать
+    return 1;
+  }
+  void ReadWord(uint8_t reg, int16_t *value) override 
+  {
+    std::cout << "Dig_T3, влажность есть (2)" << std::endl; // TODO убрать
+    *value = 1;
   }
   
   
-  uint16_t ModeReadGidT1(uint8_t reg) override 
+  void ReadWord(uint8_t reg, uint16_t *value) override 
   {
-    tx[1]={0};
-    tx[0]= reg | 0x80;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-    return static_cast<std::uint16_t> (SPI2::DR::Get());
+    std::cout << "Dig_T1, влажность есть (1)" << std::endl; // TODO убрать
+    *value = 2;
   }
-  
-  int16_t ModeReadGidT3(uint8_t reg) override 
+  void ReadWord(uint8_t reg, int32_t *value) override // TODO убрать
   {
-    tx[1]={0};
-    tx[0]= reg | 0x80;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-    return static_cast<std::int16_t> (SPI2::DR::Get());
-  }
- 
-  int32_t ModeReadTempPress(uint8_t reg) override 
-  {
-    tx[1]={0};
-    tx[0]= reg | 0x80;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-    return static_cast<std::int32_t> (SPI2::DR::Get());
-  
-  }
-  
-  
-  int16_t ModeReadHam(uint8_t reg) override 
-  {
-    tx[1]={0};
-    tx[0]= reg | 0x80;
-    GPIOB::ODR::ODR12::Low::Set();
-    SPI2::DR::Write(tx[0]);
-    while(!SPI2::SR::BSY::NotBusy::IsSet()) {}
-    GPIOB::ODR::ODR12::High::Set();
-    return static_cast<std::int16_t> (SPI2::DR::Get());
+    std::cout << "давление, темп есть (3)" << std::endl; // TODO убрать
+    *value = 3;
   }
   
   
 private:
-  uint8_t tx[1];
-  uint8_t rx[1];
- 
+
 };
 
 #endif
