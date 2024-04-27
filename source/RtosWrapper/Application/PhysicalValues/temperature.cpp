100ee43ec9a47d512b7a6d1630858ed69154a989
nvs174 <nejur02@mail.ru>
2024-04-27 16:06:30 +0300

    Update temperature.cpp

--- source/RtosWrapper/Application/PhysicalValues/temperature.cpp
+++ source/RtosWrapper/Application/PhysicalValues/temperature.cpp
@@ -4,37 +4,34 @@
 #include "idatanotifyt.h" // for IDataNotifyT
 #include "imeasurementsupdate.h" // for IMeasurementsUpdate
 #include "ifloatdataprovider.h" // for IFloatDataProvider 
-#include <iostream>
+#include <iostream> // for std::
 
 class Temperature: public IFloatDataProvider, public IMeasurementsUpdate, public IDataNotifyT
 {
   public:
-  
-
-  void UpdateCalc() override // uses IMeasurementsUpdate
-  {
-    measuredX = ((adcT / 16) - digT1);
-    measuredT = measuredX * digT1 + ((measuredX * measuredX * digT3) / 65536);
-    measuredT = measuredT / 1024;
-    std::cout << "Температатура = 25 градусов" << std::endl; // TODO удалить потом 
-    
-  }
     
   void  OnUpdate(uint16_t digRegT1, int16_t digRegT3, int32_t registerCodeT)  override  // // uses IDataNotifyT
   {
     adcT = registerCodeT;
-    
     digT1 = digRegT1;
-    
     digT3 = digRegT3;
   }
-    
-  float GetData() override  // uses IFloatDataProvider
+  
+   void UpdateCalc() override // uses IMeasurementsUpdate
   {
-    return measuredT;
+    auto measuredX = ((adcT / 16) - digT1);
+    measuredT = measuredX * digT1 + ((measuredX * measuredX * digT3) / 65536);
+    measuredT = measuredT / 1024;
+    std::cout << "Температатура = 25 градусов" << std::endl; // TODO удалить потом 
+  }
+    
+  void GetData(float* data) override // uses IFloatDataProvider
+  {
+    measuredT = 20.0f; // TODO удалить потом
+    *data = measuredT;
   }  
+  
 private:
-  float measuredX;
   float measuredT;
   uint16_t digT1;
   int16_t digT3;
