5d54b87f30750b4230d0e9e2fece742a02290f78
nvs174 <nejur02@mail.ru>
2024-04-27 16:06:32 +0300

    Update press.cpp

--- source/RtosWrapper/Application/PhysicalValues/press.cpp
+++ source/RtosWrapper/Application/PhysicalValues/press.cpp
@@ -4,25 +4,30 @@
 #include "idatanotifyp.h" // for IDataNotifyp
 #include "imeasurementsupdate.h" // for IMeasurementsUpdate
 #include "ifloatdataprovider.h" // for IFloatDataProvider 
-#include <iostream>
+#include <iostream> // for std::
 
 class Press: public IFloatDataProvider, public IMeasurementsUpdate, public IDataNotifyP
 {
 public:
   void UpdateCalc() override // uses IMeasurementsUpdate
   {
+    measuredP = (adcP / 16) * 0.18f;
     std::cout << "Давление = 5" << std::endl; // TODO удалить потом 
-    
   }
     
   void  OnUpdate(int32_t registerCodeP)  override  // uses IDataNotifyP
   {
-    
+    adcP = registerCodeP;
   }
     
-  float GetData() override  // uses IFloatDataProvider
+  void GetData(float* data) override  // uses IFloatDataProvider
   {
-   return 1;
+    measuredP = 21.0f;
+   *data = measuredP;
   } 
+private:
+   int32_t adcP;
+   float measuredP;
+    
 };
 #endif
