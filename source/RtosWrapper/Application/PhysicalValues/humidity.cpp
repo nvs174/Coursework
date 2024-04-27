479a3d7811598f1de9fa3a05be313caf0cc7968a
nvs174 <nejur02@mail.ru>
2024-04-27 16:06:34 +0300

    Update humidity.cpp

--- source/RtosWrapper/Application/PhysicalValues/humidity.cpp
+++ source/RtosWrapper/Application/PhysicalValues/humidity.cpp
@@ -1,28 +1,32 @@
 #ifndef HUMIDITY
 #define HUMIDITY
 
-#include "idatanotifyH.h" // for IDataNotifyH
+#include "idatanotifyh.h" // for IDataNotifyH
 #include "imeasurementsupdate.h" // for IMeasurementsUpdate
 #include "ifloatdataprovider.h" // for IFloatDataProvider 
-#include <iostream>
+#include <iostream> // for std::
 
 class Humidity: public IFloatDataProvider, public IMeasurementsUpdate, public IDataNotifyH
 {
 public:
   void UpdateCalc() override // uses IMeasurementsUpdate
   {
+    measuredH = adcH * 0.008;
     std::cout << "Влажность 50%" << std::endl; // TODO удалить потом 
-    
   }
     
   void  OnUpdate(int16_t registerCodeH)  override  // uses IDataNotifyH
   {
-    
+    adcH = registerCodeH;
   }
     
-  float GetData() override  // uses IFloatDataProvider
+  void GetData(float* data)  override // uses IFloatDataProvider
   {
-   return 1;
+    measuredH = 22.0f;
+    *data =  measuredH;// TODO удалить потом
   } 
+private:
+   int16_t adcH;
+   float measuredH;  
 };
 #endif
