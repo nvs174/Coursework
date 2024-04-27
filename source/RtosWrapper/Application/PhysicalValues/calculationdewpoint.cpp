c6ad160ed5650ae09550dd5e9fdc0f9e3910b679
nvs174 <nejur02@mail.ru>
2024-04-27 16:06:35 +0300

    Update calculationdewpoint.cpp

--- source/RtosWrapper/Application/PhysicalValues/calculationdewpoint.cpp
+++ source/RtosWrapper/Application/PhysicalValues/calculationdewpoint.cpp
@@ -3,21 +3,38 @@
 
 #include "imeasurementsupdate.h" // for IMeasurementsUpdate
 #include "ifloatdataprovider.h" // for IFloatDataProvider 
-#include <iostream>
+#include <iostream> // for std::
 
 class DewPoint: public IFloatDataProvider, public IMeasurementsUpdate
 {
 public:
+  DewPoint(IFloatDataProvider& dataT, IFloatDataProvider& dataH) : mdataT(dataT), mdataH(dataH) {}
   void UpdateCalc() override // uses IMeasurementsUpdate
   {
+    float measuredT;
+    float measuredH; 
+    float const a = 17.27f;
+    float const b = 237.7f;
+    float measuredY;
+        
+    mdataT.GetData(&measuredT);
+    mdataH.GetData(&measuredH);
+    measuredY = ((a * measuredT) / (b + measuredT)) + log(measuredH);
+    measuredD = (b * measuredY) / (a - measuredY);
     std::cout << "“очка росы 222" << std::endl; // TODO удалить потом 
     
   }
    
     
-  float GetData() override  // uses IFloatDataProvider
+  void GetData(float* data) override  // uses IFloatDataProviderD
   {
-   return 1;
-  } 
+    measuredD = 23.0f; // TODO удалить потом
+   *data = measuredD;
+  }
+private:
+  
+  float measuredD; 
+  IFloatDataProvider& mdataT;
+  IFloatDataProvider& mdataH;
 };
 #endif
