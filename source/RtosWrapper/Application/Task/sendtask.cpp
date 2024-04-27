99ba752778232a03a01bda34164c33bad8c86b32
nvs174 <nejur02@mail.ru>
2024-04-27 17:50:11 +0300

    Create sendtask.cpp

---
+++ source/RtosWrapper/Application/Task/sendtask.cpp
@@ -0,0 +1,23 @@
+#ifndef SENDTASK
+#define SENDTASK
+
+#include "thread.hpp" // for OsWrapper::Thread<>
+#include "itransfer.h" // for update register
+
+class SendTask: public OsWrapper::Thread<128> 
+{
+public:
+  SendTask(ITransfer& transfer) : mTransfer(transfer) {}
+  void Execute() override 
+  {
+    for (;;) 
+    {
+      mTransfer.TransferData();
+      Sleep(1000ms);
+    }
+  }
+private:
+ITransfer& mTransfer;
+};
+
+#endif
