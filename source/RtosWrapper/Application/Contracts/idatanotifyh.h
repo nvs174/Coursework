eb110bec7e2247dfab74387999492fce439456a7
nvs174 <nejur02@mail.ru>
2024-04-28 22:55:16 +0300

    лишние

--- source/RtosWrapper/Application/Contracts/idatanotifyh.h
+++
@@ -1,9 +0,0 @@
-#ifndef IDATANOTIFYH
-#define IDATANOTIFYH
-#include <iostream>
-class IDataNotifyH
-{
-public:
-  virtual void OnUpdate(int16_t registerCodeH) = 0;
-};
-#endif
--- source/RtosWrapper/Application/Contracts/idatanotifyp.h
+++
@@ -1,9 +0,0 @@
-#ifndef IDATANOTIFYP
-#define IDATANOTIFYP
-#include <iostream>
-class IDataNotifyP
-{
-public:
-  virtual void OnUpdate(int32_t registerCodeP) = 0;
-};
-#endif
