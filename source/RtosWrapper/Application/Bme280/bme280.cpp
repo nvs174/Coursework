5a9782e2e2c5cdf5e634a3c7d63eb56534b8f28f
nvs174 <nejur02@mail.ru>
2024-05-10 14:48:28 +0300

    добавил InitBme

--- source/RtosWrapper/Application/Bme280/bme280.cpp
+++ source/RtosWrapper/Application/Bme280/bme280.cpp
@@ -1,10 +1,18 @@
 #include "bme280.h"
 
+
+void Bme280::InitBme280() 
+{
+  // TODO настройка BME280 по SPI
+
+}
+
+
 void Bme280::Update() 
 {
   //TODO получить данные по SPI;
   
-  
+
   mdataT.OnUpdate(digRegT1,digRegT3,registerCodeT);
   mdataP.OnUpdate(registerCodeP);
   mdataH.OnUpdate(registerCodeH); 
